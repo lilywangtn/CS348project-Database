303d6fa18f4a67e29e8cfd332c8636358134cc32
lilywangtn <lilywangtn@gmail.com>
2024-06-07 17:06:13 -0400

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,79 +1,62 @@
 CREATE TABLE `users` (
     `uid` INT NOT NULL AUTO_INCREMENT,
     `username` VARCHAR(50) NOT NULL,
-    `password` CHAR(64) NOT NULL,
+    `password` VARCHAR(50) NOT NULL,
     `email` VARCHAR(100) NOT NULL,
     `phone` CHAR(10),
     `identity` CHAR(20) NOT NULL,
-    `has_permit` BOOL NOT NULL,
-    `bookings` INT NOT NULL DEFAULT 0,
+    `booking_num` INT NOT NULL DEFAULT 0,
     PRIMARY KEY (`uid`),
-    CONSTRAINT `c_bookings` CHECK (`bookings` <= 3),
-    CONSTRAINT `c_id_type` CHECK (`identity` IN ("staff", "admin", "visitor"))
+    CONSTRAINT `c_bookings` CHECK (`booking_num` <= 3),
+    CONSTRAINT `c_id_type` CHECK (`identity` IN ("staff", "admin", "visitor", "student"))
 );
 
-CREATE TABLE `uw_spots` (
-    `sid` INT NOT NULL AUTO_INCREMENT,
-    `location` VARCHAR(50) NOT NULL,
+CREATE TABLE `permit_holders` (
+	`uid` INT NOT NULL,
+    `type` CHAR(20) NOT NULL,
+    `start_time` TIME NOT NULL,
+    `end_time` TIME NOT NULL,
+    PRIMARY KEY (`uid`),
+    FOREIGN KEY (`uid`) REFERENCES `users`(`uid`),
+    CONSTRAINT `c_type` CHECK (`type` IN ("accessible", "staff", "student"))
+);
+
+CREATE TABLE `lots` (
+	`lid` INT NOT NULL AUTO_INCREMENT,
+    `area` CHAR(2) NOT NULL,
     `address` VARCHAR(64) NOT NULL,
+    `lot_name` VARCHAR(50) NOT NULL,
+    `capacity` INT NOT NULL,
+    PRIMARY KEY (`lid`),
+    CONSTRAINT `c_area` CHECK (`area` IN ("uw", "cw"))
+);
+
+CREATE TABLE `spots` (
+	`lid` INT NOT NULL,
+    `sid` INT NOT NULL,
     `parking_type` CHAR(20) NOT NULL,
     `latitude` VARCHAR(50) NOT NULL,
     `longitude` VARCHAR(50) NOT NULL,
     `maxstay` FLOAT,
     `price` FLOAT,
     `like_num` INT, 
-    PRIMARY KEY (`sid`,`location`),
-    CONSTRAINT `c_park_type` CHECK (`parking_type` IN ("permit", "free", "transponder"))
+    PRIMARY KEY (`lid`, `sid`),
+    FOREIGN KEY (`lid`) REFERENCES `lots`(`lid`),
+    CONSTRAINT `c_park_type` CHECK (`parking_type` IN ("permit", "pay", "accessible", "free"))
 );
 
-CREATE TABLE `cw_spots` (
-    `sid` INT NOT NULL AUTO_INCREMENT,
-    `location` VARCHAR(50) NOT NULL,
-    `address` VARCHAR(64) NOT NULL,
-    `parking_type` CHAR(20) NOT NULL,
-    `latitude` VARCHAR(50) NOT NULL,
-    `longitude` VARCHAR(50) NOT NULL,
-    `maxstay` FLOAT,
-    `price` FLOAT,
-    `like_num` INT, 
-    PRIMARY KEY (`sid`,`location`),
-    CONSTRAINT `c_park_type_cw` CHECK (`parking_type` IN ("permit", "free", "transponder"))
-);
-
-CREATE TABLE `uw_booking` (
+CREATE TABLE `bookings` (
     `bid` INT NOT NULL AUTO_INCREMENT,
     `uid` INT NOT NULL,
+    `lid` INT NOT NULL,
     `sid` INT NOT NULL,
-    `location` VARCHAR(50) NOT NULL,
-    `address` VARCHAR(64),
     `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     `start_time` TIME NOT NULL,
-    `length_of_stay` FLOAT,
+    `length_of_stay` FLOAT NOT NULL,
     `end_time` TIME NOT NULL,
-    `start_date` DATE NOT NULL,
-    `end_date` DATE NOT NULL,
     `price` FLOAT,
     `status` BOOL NOT NULL,
     PRIMARY KEY (`bid`),
     FOREIGN KEY (`uid`) REFERENCES `users`(`uid`),
-    FOREIGN KEY (`sid`) REFERENCES `uw_spots`(`sid`)
-);
-
-CREATE TABLE `cw_booking` (
-    `bid` INT NOT NULL AUTO_INCREMENT,
-    `uid` INT NOT NULL,
-    `sid` INT NOT NULL,
-    `location` VARCHAR(50) NOT NULL,
-    `address` VARCHAR(64),
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    `start_time` TIME NOT NULL,
-    `length_of_stay` FLOAT,
-    `end_time` TIME NOT NULL,
-    `start_date` DATE NOT NULL,
-    `end_date` DATE NOT NULL,
-    `price` FLOAT,
-    `status` BOOL NOT NULL,
-    PRIMARY KEY (`bid`),
-    FOREIGN KEY (`uid`) REFERENCES `users`(`uid`),
-    FOREIGN KEY (`sid`) REFERENCES `cw_spots`(`sid`)
+    FOREIGN KEY (`lid`,`sid`) REFERENCES `spots`(`lid`,`sid`)
 );
