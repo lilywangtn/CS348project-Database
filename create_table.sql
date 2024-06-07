2db316b571da201862a38b975a7ff3bfdb52d029
lilywangtn <lilywangtn@gmail.com>
2024-06-07 17:15:54 -0400

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,21 +1,21 @@
 CREATE TABLE `users` (
     `uid` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(50) NOT NULL,
-    `password` VARCHAR(50) NOT NULL,
-    `email` VARCHAR(100) NOT NULL,
+    `username` VARCHAR(16) NOT NULL,
+    `password` VARCHAR(16) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
     `phone` CHAR(10),
-    `identity` CHAR(20) NOT NULL,
+    `identity` CHAR(10) NOT NULL,
     `booking_num` INT NOT NULL DEFAULT 0,
     PRIMARY KEY (`uid`),
     CONSTRAINT `c_bookings` CHECK (`booking_num` <= 3),
-    CONSTRAINT `c_id_type` CHECK (`identity` IN ("staff", "admin", "visitor", "student"))
+    CONSTRAINT `c_id_type` CHECK (`identity` IN ("admin", "staff", "visitor", "student"))
 );
 
 CREATE TABLE `permit_holders` (
     `uid` INT NOT NULL,
     `type` CHAR(20) NOT NULL,
-    `start_time` TIME NOT NULL,
-    `end_time` TIME NOT NULL,
+    `start_time` DATETIME NOT NULL,
+    `end_time` DATETIME NOT NULL,
     PRIMARY KEY (`uid`),
     FOREIGN KEY (`uid`) REFERENCES `users`(`uid`),
     CONSTRAINT `c_type` CHECK (`type` IN ("accessible", "staff", "student"))
@@ -27,6 +27,7 @@ CREATE TABLE `lots` (
     `address` VARCHAR(64) NOT NULL,
     `lot_name` VARCHAR(50) NOT NULL,
     `capacity` INT NOT NULL,
+    `like_num` INT, 
     PRIMARY KEY (`lid`),
     CONSTRAINT `c_area` CHECK (`area` IN ("uw", "cw"))
 );
@@ -37,9 +38,8 @@ CREATE TABLE `spots` (
     `parking_type` CHAR(20) NOT NULL,
     `latitude` VARCHAR(50) NOT NULL,
     `longitude` VARCHAR(50) NOT NULL,
-    `maxstay` FLOAT,
+    `max_stay` FLOAT,
     `price` FLOAT,
-    `like_num` INT, 
     PRIMARY KEY (`lid`, `sid`),
     FOREIGN KEY (`lid`) REFERENCES `lots`(`lid`),
     CONSTRAINT `c_park_type` CHECK (`parking_type` IN ("permit", "pay", "accessible", "free"))
@@ -51,9 +51,9 @@ CREATE TABLE `bookings` (
     `lid` INT NOT NULL,
     `sid` INT NOT NULL,
     `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    `start_time` TIME NOT NULL,
+    `start_time` DATETIME NOT NULL,
     `length_of_stay` FLOAT NOT NULL,
-    `end_time` TIME NOT NULL,
+    `end_time` DATETIME NOT NULL,
     `price` FLOAT,
     `status` BOOL NOT NULL,
     PRIMARY KEY (`bid`),
